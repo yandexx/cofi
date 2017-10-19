e496692d233c5ce0d204c9f46a24e05518310c10
Vsevolod Zubarev <vsevolod.zubarev@veeam.com>
2017-10-19 19:32:48 +0300

    Initial commit, v0.1.0.

---
+++ src/converter.rs
@@ -0,0 +1,46 @@
+pub fn literal_to_bytes(input: &str) -> Result<u64, String> {
+    match input.parse::<u64>() {
+        Ok(result) => Ok(result),
+        Err(why) => {
+            let (num_str, suffix) = input.split_at(input.len() - 1);
+            let num = match num_str.parse::<u64>() {
+                Ok(num) => num,
+                Err(why) => return Err(format!("Can't parse \"{}\": {}.", num_str, why)), 
+            };
+            match suffix {
+                "K" => Ok(num * 1024),
+                "M" => Ok(num * 1024 * 1024),
+                "G" => Ok(num * 1024 * 1024 * 1024),
+                "T" => Ok(num * 1024 * 1024 * 1024 * 1024),
+                _ => {
+                    Err(format!(
+                        "Cannot convert: {}, or unknown suffix. Try K, M, G or T.",
+                        why
+                    ))
+                }
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::literal_to_bytes;
+
+    #[test]
+    fn digits() {
+        assert_eq!(Ok(1234), literal_to_bytes("1234"));
+    }
+    #[test]
+    fn kilos() {
+        assert_eq!(Ok(4096), literal_to_bytes("4K"));
+    }
+    #[test]
+    fn megas() {
+        assert_eq!(Ok(36700160), literal_to_bytes("35M"));
+    }
+    #[test]
+    fn gigas() {
+        assert_eq!(Ok(119185342464), literal_to_bytes("111G"));
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,100 @@
+#[macro_use]
+extern crate error_chain;
+extern crate rand; 
+extern crate md5;
+extern crate winapi;
+
+mod converter;
+use converter::literal_to_bytes; 
+
+use std::io::prelude::*;
+use std::fs::OpenOptions;
+use std::os::windows::prelude::*;
+use rand::{thread_rng, Rng};
+
+error_chain! {
+    foreign_links {
+        Io(std::io::Error);
+        Convert(std::num::ParseIntError);
+    }
+} 
+
+fn run() -> Result<()> {
+    let cmd_args: Vec<String> = std::env::args().collect(); 
+    if cmd_args.len() != 4 {
+        println!("cofi -- corruption finder");
+        println!("Usage: cofi block_size file_size \"path\"");
+        println!();
+        println!("Example: cofi 4M 1G c:\\testfile.dat");
+        return Ok(())
+    }
+
+    let block_size = literal_to_bytes(&cmd_args[1])? as usize;
+    let file_size = literal_to_bytes(&cmd_args[2])? as usize;
+    let blocks_total = if file_size % block_size > 0 {
+        file_size / block_size + 1
+    } else {
+        file_size / block_size
+    };
+    let path = &cmd_args[3];
+
+    println!("Writing {} blocks of size {} bytes to file \"{}\"", blocks_total, block_size, path);
+
+    let mut check_sums_src: Vec<md5::Digest> = Vec::with_capacity(blocks_total);    
+    let mut check_sums_trg: Vec<md5::Digest> = Vec::with_capacity(blocks_total);    
+
+    let mut iteration = 0;
+    loop {
+        {
+            let mut pre_block: Vec<u8> = vec![0; block_size];
+
+            let mut file = OpenOptions::new()
+                .create(true)
+                .write(true)
+                .truncate(true)
+                .custom_flags(winapi::FILE_FLAG_NO_BUFFERING | winapi::FILE_FLAG_WRITE_THROUGH)
+                .open(path)?;
+
+            for _ in 0..blocks_total {
+                thread_rng().fill_bytes(&mut pre_block);
+                let data_block = pre_block.clone();
+                if data_block != pre_block {
+                    panic!("Corruption in memory. Cloned block doesn't match the source");
+                }
+                let digest = md5::compute(&data_block);
+                check_sums_src.push(digest);
+
+                file.write(&data_block)?;
+            }
+        }
+
+        {
+            let mut data_block: Vec<u8> = vec![0; block_size];
+    
+            let mut file = OpenOptions::new()
+                .read(true)
+                .custom_flags(winapi::FILE_FLAG_NO_BUFFERING)
+                .open(path)?;
+
+            for _ in 0..blocks_total {
+                file.read(&mut data_block)?;
+                let digest = md5::compute(&data_block);
+                check_sums_trg.push(digest);
+            }
+        }
+
+        for (i, _) in check_sums_src.iter().enumerate() {
+            if *check_sums_src[i] != *check_sums_trg[i] {
+                println!("MD5 mismatch in block {}!", i);
+                println!("Original md5: \"{:x}\", target md5: \"{:x}\".", check_sums_src[i], check_sums_trg[i]);
+                panic!("Data got corrupted.");
+            }
+        }
+        println!("Iteration {} OK", iteration);
+        iteration += 1;
+        check_sums_src.clear();
+        check_sums_trg.clear();
+    }
+}
+
+quick_main!(run);
